945089ae7b77c8c63aaebfb3aac1687e44edd8b1
Matt Kucenski <mkucenski@me.com>
2021-09-04 17:32:03 -0700

    Forcing rules to look for presence of netflow field so the pipeline doesn't have to care what types of data flows through

--- data-dictionary/scripts/netflow-graylog.sh
+++ data-dictionary/scripts/netflow-graylog.sh
@@ -4,8 +4,15 @@ YAML="$1"
 
 echo "rule \"normalize_netflow\""
 echo "when"
-echo "  true"
+echo "  has_field(\"nf_src\")"
 echo "then"
+echo "  set_field(\"event_source\", \"netflow\");"
+echo "  set_field(\"event_type\", \"flow\");"
+echo
+echo "  set_field(\"incident_number\", \"\");"
+echo "  set_field(\"incident_title\", \"\");"
+echo "  set_field(\"incident_dataset\", \"\");"
+echo
 
 yq '.event_fields[] | "  rename_field(\"" + .graylog_name + "\", \"" + .standard_name + "\");"' "$YAML" | sed -r 's/^"//; s/"$//; s/\\"/"/g'
 
