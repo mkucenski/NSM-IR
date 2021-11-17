f333a4ed1cbbe7a1d1ccd218c8e6d68afd6b56f1
Matt Kucenski <mkucenski@me.com>
2021-11-17 10:56:03 -0800

    Updating to use Suricata 6x and cleanup on script

--- suricata/suricata.sh
+++ suricata/suricata.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
+. "${BASH_SOURCE%/*}/../../scripts/common-include.sh" || exit 1
 
 PCAP_DIR="$1"
 OUTPUT_DIR="$2"
 
-suricata -c /usr/local/etc/suricata/suricata.yaml -r "$PCAP_DIR" -l "$OUTPUT_DIR"
+if [ $# -eq 0 ]; then
+	USAGE "PCAP_DIR" "OUTPUT_DIR"
+	USAGE_DESCRIPTION "This script executes Suricata against the specified PCAP_DIR and places all log files in OUTPUT_DIR."
+ 	USAGE_EXAMPLE "$(basename "$0") /data/pcap/ /data/suricata/"
+	exit 1
+fi
+
+suricata -c "${BASH_SOURCE%/*}/suricata-6x.yaml" -r "$PCAP_DIR" -l "$OUTPUT_DIR"
 
